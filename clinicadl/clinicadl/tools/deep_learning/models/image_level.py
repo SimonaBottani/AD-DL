5d0f7f0192eb80a53f6b0b9b592ae79852adfe5a
BOTTANI Simona <simonabottani92@gmail.com>
2020-11-01 20:20:10 +0100

    Add different model of multitask

--- clinicadl/clinicadl/tools/deep_learning/models/image_level.py
+++ clinicadl/clinicadl/tools/deep_learning/models/image_level.py
@@ -148,6 +148,181 @@ class Conv5_FC3_MultiTask(nn.Module):
 
         return out1, out2, out3, out4
 
+class Conv5_FC3_MultiTask_2_2(nn.Module):
+    """
+    Classifier for a multi-task classification task
+
+    Image level architecture used on Minimal preprocessing
+    """
+    def __init__(self, dropout=0.5):
+        super(Conv5_FC3_MultiTask_2_2, self).__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv3d(1, 8, 3, padding=1),
+            nn.BatchNorm3d(8),
+            nn.ReLU(),
+            PadMaxPool3d(2, 2),
+
+            nn.Conv3d(8, 16, 3, padding=1),
+            nn.BatchNorm3d(16),
+            nn.ReLU(),
+            PadMaxPool3d(2, 2),
+
+            nn.Conv3d(16, 32, 3, padding=1),
+            nn.BatchNorm3d(32),
+            nn.ReLU(),
+            PadMaxPool3d(2, 2),
+
+            nn.Conv3d(32, 64, 3, padding=1),
+            nn.BatchNorm3d(64),
+            nn.ReLU(),
+            PadMaxPool3d(2, 2),
+
+            nn.Conv3d(64, 128, 3, padding=1),
+            nn.BatchNorm3d(128),
+            nn.ReLU(),
+            PadMaxPool3d(2, 2),
+
+        )
+
+        self.classifier3 = nn.Sequential(
+            Flatten(),
+            nn.Dropout(p=dropout),
+            nn.Linear(128 * 6 * 7 * 6, 1300),
+            nn.ReLU(),
+            nn.Linear(1300, 50),
+            nn.ReLU(),
+            nn.Linear(50, 2)
+        )
+
+
+
+        self.flattened_shape = [-1, 128, 6, 7, 6]
+
+    def forward(self, x):
+        x = self.features(x) # the same for all the tasks
+        out1 = self.classifier3(x)
+        out2 = self.classifier3(x)
+
+        return out1, out2
+
+class Conv5_FC3_MultiTask_3_2(nn.Module):
+    """
+    Classifier for a multi-task classification task
+
+    Image level architecture used on Minimal preprocessing
+    """
+    def __init__(self, dropout=0.5):
+        super(Conv5_FC3_MultiTask_3_2, self).__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv3d(1, 8, 3, padding=1),
+            nn.BatchNorm3d(8),
+            nn.ReLU(),
+            PadMaxPool3d(2, 2),
+
+            nn.Conv3d(8, 16, 3, padding=1),
+            nn.BatchNorm3d(16),
+            nn.ReLU(),
+            PadMaxPool3d(2, 2),
+
+            nn.Conv3d(16, 32, 3, padding=1),
+            nn.BatchNorm3d(32),
+            nn.ReLU(),
+            PadMaxPool3d(2, 2),
+
+            nn.Conv3d(32, 64, 3, padding=1),
+            nn.BatchNorm3d(64),
+            nn.ReLU(),
+            PadMaxPool3d(2, 2),
+
+            nn.Conv3d(64, 128, 3, padding=1),
+            nn.BatchNorm3d(128),
+            nn.ReLU(),
+            PadMaxPool3d(2, 2),
+
+        )
+
+        self.classifier3 = nn.Sequential(
+            Flatten(),
+            nn.Dropout(p=dropout),
+            nn.Linear(128 * 6 * 7 * 6, 1300),
+            nn.ReLU(),
+            nn.Linear(1300, 50),
+            nn.ReLU(),
+            nn.Linear(50, 2)
+        )
+
+
+
+        self.flattened_shape = [-1, 128, 6, 7, 6]
+
+    def forward(self, x):
+        x = self.features(x) # the same for all the tasks
+        out1 = self.classifier3(x)
+        out2 = self.classifier3(x)
+        out3 = self.classifier3(x)
+
+        return out1, out2, out3
+
+class Conv5_FC3_MultiTask_4_2(nn.Module):
+    """
+    Classifier for a multi-task classification task
+
+    Image level architecture used on Minimal preprocessing
+    """
+    def __init__(self, dropout=0.5):
+        super(Conv5_FC3_MultiTask_4_2, self).__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv3d(1, 8, 3, padding=1),
+            nn.BatchNorm3d(8),
+            nn.ReLU(),
+            PadMaxPool3d(2, 2),
+
+            nn.Conv3d(8, 16, 3, padding=1),
+            nn.BatchNorm3d(16),
+            nn.ReLU(),
+            PadMaxPool3d(2, 2),
+
+            nn.Conv3d(16, 32, 3, padding=1),
+            nn.BatchNorm3d(32),
+            nn.ReLU(),
+            PadMaxPool3d(2, 2),
+
+            nn.Conv3d(32, 64, 3, padding=1),
+            nn.BatchNorm3d(64),
+            nn.ReLU(),
+            PadMaxPool3d(2, 2),
+
+            nn.Conv3d(64, 128, 3, padding=1),
+            nn.BatchNorm3d(128),
+            nn.ReLU(),
+            PadMaxPool3d(2, 2),
+
+        )
+
+        self.classifier3 = nn.Sequential(
+            Flatten(),
+            nn.Dropout(p=dropout),
+            nn.Linear(128 * 6 * 7 * 6, 1300),
+            nn.ReLU(),
+            nn.Linear(1300, 50),
+            nn.ReLU(),
+            nn.Linear(50, 2)
+        )
+
+        self.flattened_shape = [-1, 128, 6, 7, 6]
+
+    def forward(self, x):
+        x = self.features(x) # the same for all the tasks
+        out1 = self.classifier3(x)
+        out2 = self.classifier3(x)
+        out3 = self.classifier3(x)
+        out4 = self.classifier3(x)
+
+        return out1, out2, out3, out4
+
 class Conv5_FC3_4_classes(nn.Module):
     """
     Classifier for a binary classification task
