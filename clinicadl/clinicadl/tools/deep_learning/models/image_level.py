0ef9cc9574f4e650b9d1fcb3a8642f1e8f9e0c6d
BOTTANI Simona <simonabottani92@gmail.com>
2020-12-21 10:53:08 +0100

    Print to test

--- clinicadl/clinicadl/tools/deep_learning/models/image_level.py
+++ clinicadl/clinicadl/tools/deep_learning/models/image_level.py
@@ -598,7 +598,10 @@ class GeneratorUNet(nn.Module):
         d5 = self.down5(d4)
 
         u1 = self.up1(d5)
-        print(u1.shape())
+        print('########### U1 ###############')
+        print(u1)
+        print('########### d4 ###############')
+        print(d4)
         u2 = self.up2(u1, d4)
         u3 = self.up3(u2, d3)
         u4 = self.up4(u3, d2)
